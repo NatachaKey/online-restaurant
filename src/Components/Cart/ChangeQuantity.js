21bfc566cfef01b8cf8d25e91ade51abccb6a4f0
Natacha Key <103755090+NatachaKey@users.noreply.github.com>
2022-10-07 11:44:18 +0200

    Update ChangeQuantity.js

--- src/Components/Cart/ChangeQuantity.js
+++ src/Components/Cart/ChangeQuantity.js
@@ -15,9 +15,9 @@ const ChangeQuantity = ({quantity, setQuantity})=>{
 
     return(<div>
         
-        <button onClick={removeQuantity}>-</button>
-        <span>{quantity}</span>   
-        <button onClick={addQuantity}>+</button>
+        <button onClick={removeQuantity}>- </button>
+        <span> {quantity} </span>   
+        <button onClick={addQuantity}> +</button>
       
     </div>)
 }
