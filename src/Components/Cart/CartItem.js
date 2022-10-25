9d6a1d809e4583300404f814fbf7f667d992222e
Natacha Key <103755090+NatachaKey@users.noreply.github.com>
2022-10-25 20:51:44 +0200

    Update CartItem.js

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -3,17 +3,17 @@ import { useDispatch } from 'react-redux';
 import dataDishes from '../../data/dataDishes';
 import { removeItemFromCart } from '../../redux/cartSlice';
 
-const CartItem =({cartItem})=>{
+const CartItem =({ cartItem })=>{
    
 
     const dishes= dataDishes.find(item => item.id === cartItem.dishId);
     const dispatch= useDispatch();
     
     return ( <div>
-        <p>{dishes.name}</p>
-       <p> {cartItem.quantity} portion(s)</p>
-       <p>Price: $ {dishes.price * cartItem.quantity}</p>
-       <span onClick ={()=>dispatch(removeItemFromCart({cartItemId: cartItem.id}))}>
+       <p> { dishes.name } </p>
+       <p> { cartItem.quantity } portion(s)</p>
+       <p>Price: $ { dishes.price * cartItem.quantity }</p>
+       <span onClick ={ ()=> dispatch(removeItemFromCart({ cartItemId: cartItem.id }))}>
        
          <img className="icon" src="https://img.icons8.com/material-outlined/48/000000/trash--v1.png" alt="food"/>  
        </span>
